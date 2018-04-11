7312cadfdded973a198d0c44f76a9f5a0758fdaa
ccondie <craig.a.condie@gmail.com>
2018-04-11 00:06:52 -0600

    Working Encrypter Class. No Tests

---
+++ DocumentEncrypter.App/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DocumentEncrypter.App
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Encrypter _encrypter;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            System.IO.DirectoryInfo directory = new System.IO.DirectoryInfo(@"c:\Temp");
+            //   _encrypter = new Encrypter(directory);
+            _encrypter = new Encrypter(new System.IO.FileInfo("c:\\Temp\\privateKey.xml"), directory);
+            //_encrypter.GenerateKeys();
+            //_encrypter.Encrypt(new System.IO.FileInfo(@"c:\Temp\Sample.txt"));
+            _encrypter.Decrypt(new System.IO.FileInfo(@"c:\Temp\Sample.txt.enc"));
+        }
+    }
+}
---
+++ DocumentEncrypter/Encrypter.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using System.IO;
+
+namespace DocumentEncrypter
+{
+    public class Encrypter
+    {
+        private RSACng _rsa;
+        private RSAParameters _privateKey;
+        private RSAParameters _publicKey;
+        private DirectoryInfo _directory;
+
+        public Encrypter(DirectoryInfo directory)
+        {
+            _rsa = new RSACng(4096);
+            _privateKey = _rsa.ExportParameters(true);
+            _publicKey = _rsa.ExportParameters(false);
+            _directory = directory;
+        }
+        public Encrypter(FileInfo publicKey, DirectoryInfo directory)
+        {
+            _rsa = new RSACng(4096);
+            string key = File.ReadAllText(publicKey.FullName);
+            _rsa.FromXmlString(key);
+            _directory = directory;
+        }
+ 
+        public void Encrypt(FileInfo filePath)
+        {
+            byte[] bytes = File.ReadAllBytes(filePath.FullName);
+            byte[] encrypted = _rsa.Encrypt(bytes,RSAEncryptionPadding.Pkcs1);
+            File.WriteAllBytes(filePath.FullName + ".enc",encrypted);
+        }
+        public void Decrypt(FileInfo filePath)
+        {
+            byte[] bytes = File.ReadAllBytes(filePath.FullName);
+            byte[] decrypted = _rsa.Decrypt(bytes, RSAEncryptionPadding.Pkcs1);
+            File.WriteAllBytes(filePath.FullName.Replace(".enc",""),decrypted);
+        }
+
+        public void GenerateKeys()
+        {
+            string privateKey = _rsa.ToXmlString(true);
+            string publicKey = _rsa.ToXmlString(false);
+
+            if (_directory.Exists)
+            {
+                File.WriteAllText(_directory.FullName + @"\privateKey.xml", privateKey);
+                File.WriteAllText(_directory.FullName + @"\publicKey.xml", publicKey);
+            }
+        }
+    }
+}
